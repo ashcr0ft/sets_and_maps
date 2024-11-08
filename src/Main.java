e88eea220cc0cabbce0e86217aed72628e2c190d
mlstlit <Litvinov.v@mail.ru>
2024-11-08 22:02:35 +0300

    after the videotape

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,11 +9,13 @@ public class Main {
         taskThree();
         System.out.println("------------------------");
         taskFour();
+        System.out.println("------------------------");
+        taskFourFromSky();
     }
 
     public static void taskOne() {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        for (Integer num : nums) {
+        for (int num : nums) {
             if (num % 2 != 0) {
                 System.out.println(num);
             }
@@ -25,6 +24,7 @@ public class Main {
 
     public static void taskTwo() {
         List<Integer> nums = new ArrayList<>(List.of(6, 11, 2, 3, 8, 4, 10, 5, 7, 7, 6, 22, 5));
+        // Забыл про Collections.sort(nums);
         for (int i = 0; i < nums.size(); i++) {
             for (int j = 0; j < nums.size(); j++) {
                 if (nums.get(i) < nums.get(j)) {
@@ -44,6 +44,7 @@ public class Main {
     }
 
     public static void taskThree() {
+
         Set<String> words = new HashSet<>(List.of("Привет, Андрей!",
                 "Привет, Андрей!",
                 "Привет, Андрей",
@@ -59,7 +60,7 @@ public class Main {
 
     public static void taskFour() {
 
-        List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три", "два", "три", "один", "два"));
+        List<String> strings = new ArrayList<>(List.of("один", "один", "один", "один", "один", "один"));
         Set<String> sort = new HashSet<>(strings);
         int counter = 0;
         for (String s : sort) {
@@ -72,4 +73,21 @@ public class Main {
             counter = 0;
         }
     }
+
+    //красивое решение...
+    public static void taskFourFromSky() {
+
+        List<String> strings = new ArrayList<>(List.of("один", "два", "один", "один", "один", "один", "один"));
+
+        Map<String, Integer> wordCounts = new HashMap<>();
+
+        for (String word : strings) {
+            wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+        }
+
+        for (int count : wordCounts.values()) {
+            System.out.println(count);
+        }
+    }
 }
+
